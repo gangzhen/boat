b30ebd9fc553b244bec65dac3fad1a34c079f61d
gangzhen <gangzhen2002@gmail.com>
2024-02-12 02:32:29 +0800

    Initial Commit Frontend Code

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = "赛艇皮划艇赛事管理系统";
+      return args;
+    })
+  }
+})
